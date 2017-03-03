d69d6c1d7a4f92b13885f5da599fab722534e9a7
Diego <ingelectronicadj@gmail.com>
2017-03-03 18:03:18 -0500

    Se añade animacion de nodo por teclado teniendo en cuenta escalas

---
+++ miScript.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var posicion
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	#posicion = get_pos()
+	posicion = get_node("Diego").get_pos()
+	set_process(true)
+	pass
+
+func _process(delta):
+	if(Input.is_key_pressed(KEY_LEFT)):
+		posicion.x -= 10
+	if(Input.is_key_pressed(KEY_RIGHT)):
+		posicion.x += 10
+	if(Input.is_key_pressed(KEY_UP)): #Arriba es hacia abajo
+		posicion.y -= 3
+	if(Input.is_key_pressed(KEY_DOWN)):
+		posicion.y += 3
+	get_node("Diego").set_pos(posicion)
+	zoom()
+	
+func zoom():
+	var escala=0.0066*posicion.y - 1.06637
+	var vect = Vector2(escala,escala)
+	get_node("Diego").set_scale(vect)
